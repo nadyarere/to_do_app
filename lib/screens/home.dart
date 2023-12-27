a576c89a8685022d9ee720fad243dff915acc216
Nadya <nadya.rere@gmail.com>
2023-12-27 12:45:56 +0700

    first commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(child: Image.asset('assets/images/avatar.jpeg'),),
+          )
+        ]),
+      ),
+      body: Container(
+        child: Text('This is Home screen'),
+      ),
+    );
+  }
+}
